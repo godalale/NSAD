f595874b2369e0483a0f41f302043ec1aaceb181
Alexander Godál <godalalexander@gmail.com>
2024-07-04 13:31:48 +0200

    pridane log-verzie a shell scripty na spustenie

---
+++ runconf.sh
@@ -0,0 +1,8 @@
+echo "Running config00"
+./program config00.conf config00.root > config00.out
+echo "Running config01"
+./program config01.conf config01.root > config01.out
+echo "Running config10"
+./program config10.conf config10.root > config10.out
+echo "Running config11"
+./program config11.conf config11.root > config11.out
